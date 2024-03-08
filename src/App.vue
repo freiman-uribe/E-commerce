75326ebf12762f7047995aa4e0decd4c2c4c39e2
freiman-uribe <106196539+freiman-uribe@users.noreply.github.com>
2024-03-08 16:58:00 -0500

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <router-view />
+  </div>
+</template>
+
+<style>
+.container {
+  margin: 50px 80px;
+  display: grid;
+  justify-content: center;
+  font-family: Arial, sans-serif;
+}
+</style>
